12c01b035608e4885bb123df46dc7f574538c5e9
DarsVader666 <2664817705@qq.com>
2026-01-15 20:26:17 +0800

    First

---
+++ nre/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/d1c/nre/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/d1c/re/devel;/opt/ros/noetic'.split(';') if '/home/d1c/re/devel;/opt/ros/noetic' != '' else []
---
+++ nre/build/trajectory_planner/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/d1c/nre/src/trajectory_planner
+# Build directory: /home/d1c/nre/build/trajectory_planner
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
